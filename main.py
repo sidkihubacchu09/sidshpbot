7f79c8dfd67a28332d896aeed86a1f124abb5f57
sidkihubacchu09 <batosmodiss93@gmail.com>
2026-05-22 15:21:28 +0530

    main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from telethon import TelegramClient, events
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 API_ID = "38843772" 
 API_HASH = "875fbb273801c8025d05e98173fca536"
-BOT_TOKEN = "8637135798:AAEdTzCnL3fn1keuLzLxQN0BUULXlTMicV"
+BOT_TOKEN = ""
 
 # --- Attractive Logging Setup ---
 logging.basicConfig(level=logging.INFO, format='%(asctime)s ⚡ %(message)s', datefmt='%H:%M:%S')
